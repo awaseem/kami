1e1f32d230a206b8bda8cc3c9949f74037a394ea
Ali Waseem <github@aliwaseem.com>
2022-11-15 22:19:27 -0700

    removed get notion auth url

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import { createSlackApp } from './slack/app'
-import { getNotionAuthUrl, getPort } from './utils/env'
+import { getPort } from './utils/env'
 
 async function main() {
   const port = getPort()
   const app = createSlackApp()
-
-  const notionAuthUrl = getNotionAuthUrl()
-  console.log('Notion auth url: ' + notionAuthUrl)
 
   await app.start(port)
   console.log(`⚡️ Bolt app is running on port: ${port}`)
