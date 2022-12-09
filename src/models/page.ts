9cdac4a0797d2d2360808420a69d8add4ca16cc6
Ali Waseem <github@aliwaseem.com>
2022-12-08 19:31:51 -0700

    removed usage of content

--- src/models/page.ts
+++ src/models/page.ts
@@ -1,14 +1,6 @@
 import { createNotionClient } from '../lib/notion'
 import { NotionError } from '../utils/error'
 
-export interface CreatePageWithContentArgs {
-  accessToken: string
-  parentId: string
-  heading: string
-
-  content: string
-}
-
 export interface CreatePageFromPromptArgs {
   accessToken: string
   parentId: string
@@ -25,55 +17,6 @@ export interface CreateSummaryFromThreadArgs {
 
   threadLink: string
   content: string
-}
-
-async function createPageWithContent({
-  accessToken,
-  parentId,
-  heading,
-  content,
-}: CreatePageWithContentArgs) {
-  try {
-    const notion = createNotionClient(accessToken)
-
-    const response = await notion.pages.create({
-      parent: {
-        type: 'page_id',
-        page_id: parentId,
-      },
-      icon: { type: 'emoji', emoji: '🔮' },
-      properties: {
-        title: {
-          title: [
-            {
-              text: {
-                content: heading,
-              },
-            },
-          ],
-        },
-      },
-      children: [
-        {
-          object: 'block',
-          paragraph: {
-            rich_text: [
-              {
-                text: {
-                  content,
-                },
-              },
-            ],
-            color: 'default',
-          },
-        },
-      ],
-    })
-
-    return response
-  } catch (error) {
-    throw new NotionError(error as Error)
-  }
 }
 
 async function createPromptPage({
@@ -230,7 +173,6 @@ async function createSummaryFromThread({
 
 export function createPageModel() {
   return Object.freeze({
-    createPageWithContent,
     createPromptPage,
     createSummaryFromThread,
   })
