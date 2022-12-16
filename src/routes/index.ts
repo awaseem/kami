218cb15e01c694185231fbd35de3c08366e16ba5
Ali Waseem <github@aliwaseem.com>
2022-12-15 20:50:31 -0700

    updated saas based listing for auth

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,18 +3,29 @@ import { Controllers } from '../controllers'
 import {
   ENV_slackClientId,
   ENV_slackClintSecret,
+  ENV_slackSecretToken,
   ENV_slackSigningSecret,
   ENV_slackStateSecret,
+  saasBased,
 } from '../utils/env'
 import { SLACK_SCOPES } from '../utils/slack'
 
 export function createRouter(controllers: Controllers) {
+  const auth = saasBased
+    ? {
+        clientId: ENV_slackClientId,
+        clientSecret: ENV_slackClintSecret,
+        signingSecret: ENV_slackSigningSecret,
+        stateSecret: ENV_slackStateSecret,
+        scopes: SLACK_SCOPES,
+      }
+    : {
+        token: ENV_slackSecretToken,
+        signingSecret: ENV_slackSigningSecret,
+      }
+
   const receiver = new ExpressReceiver({
-    clientId: ENV_slackClientId,
-    clientSecret: ENV_slackClintSecret,
-    signingSecret: ENV_slackSigningSecret,
-    stateSecret: ENV_slackStateSecret,
-    scopes: SLACK_SCOPES,
+    ...auth,
 
     installerOptions: {
       directInstall: true,
